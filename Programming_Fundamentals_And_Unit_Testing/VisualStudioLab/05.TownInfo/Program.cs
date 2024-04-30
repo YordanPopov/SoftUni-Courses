1a7af025f27c60445d12b9a2fcf2d7367b89e6fb
YordanPopov <jordanpopov190@gmail.com>
2024-04-30 18:34:02 +0300

    Upload VisualStudioLab solution

---
+++ Programming_Fundamentals_And_Unit_Testing/VisualStudioLab/05.TownInfo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _05.TownInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            string country = Console.ReadLine();
+
+            Console.WriteLine($"{town} is in {country}");
+        }
+    }
+}

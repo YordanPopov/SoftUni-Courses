33de6d98203ac8985ee351aab3229ea6978764e0
YordanPopov <jordanpopov190@gmail.com>
2024-05-12 14:13:30 +0300

    Update exercise AccountBalance

--- Programming_Fundamentals_And_Unit_Testing/14.ExerciseLoops/08.AccountBalance/Program.cs
+++ Programming_Fundamentals_And_Unit_Testing/14.ExerciseLoops/08.AccountBalance/Program.cs
@@ -12,16 +12,16 @@
                 double money = double.Parse(input);
                 if (money > 0)
                 {
-                    Console.WriteLine($"Increase: {money:f2}");
+                    Console.WriteLine($"Increase: {Math.Abs(money):f2}");
                     balance += money;
                 }else
                 {
-                    Console.WriteLine($"Decrease: {money:f2}");
+                    Console.WriteLine($"Decrease: {Math.Abs(money):f2}");
                     balance += money;
                 }
                 input = Console.ReadLine();
             }
-            Console.WriteLine($"{balance:f2}");
+            Console.WriteLine($"Balance: {balance:f2}");
         }
     }
 }

640afa26a89ed687c9f13b2fbedea0bcbf6d9f17
YordanPopov <jordanpopov190@gmail.com>
2024-06-04 22:15:04 +0300

    Upload ArraysAndListsExercise solution

---
+++ Programming_Fundamentals_And_Unit_Testing/20.ArraysAndListsExercise/03.MergingLists/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _03.MergingLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers1 = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> numbers2 = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> output = new List<int>();
+
+            int longerList = Math.Max(numbers1.Count, numbers2.Count);
+
+            for (int i = 0; i < longerList; i++)
+            {
+                if (i < numbers1.Count)
+                {
+                    output.Add(numbers1[i]);
+                }
+
+                if (i < numbers2.Count)
+                {
+                    output.Add(numbers2[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", output));
+        }
+    }
+}
---
+++ Programming_Fundamentals_And_Unit_Testing/20.ArraysAndListsExercise/05.BombNumbers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _05.BombNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> bombNumbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int bombNum = bombNumbers[0];
+            int pow = bombNumbers[1];
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == bombNum)
+                {
+                    int startIndex = i - pow;
+                    int range = pow + pow + 1;
+
+                    if (startIndex < 0)
+                    {
+                        range += startIndex;
+                        startIndex = 0;
+                    }
+
+                    if (startIndex + range > numbers.Count)
+                    {
+                        range = numbers.Count - startIndex;
+                    }
+
+                    numbers.RemoveRange(startIndex, range);
+                    i = -1;
+                }
+                
+            }
+
+            Console.WriteLine($"{numbers.Sum()}");
+        }
+    }
+}

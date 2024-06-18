c39bedd17e4cd111332bdde4366baa6a7ba06163
YordanPopov <jordanpopov190@gmail.com>
2024-06-18 22:35:26 +0300

    Upload CenterPoint and FoldArray Solutions

---
+++ Programming_Fundamentals_And_Unit_Testing/ExamPreparation/03-Center-Point-Resources/03-Center-Point-Resources/TestApp.Tests/CenterPointTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class CenterPointTests
+{
+    [Test]
+    public void Test_GetClosest_WhenFirstPointIsCloser_ShouldReturnFirstPoint()
+    {
+        // Arrange
+        double x1 = 1d;
+        double y1 = 1d;
+        double x2 = 2d;
+        double y2 = 2d;
+        string expectedResult = "(1, 1)";
+
+        // Act
+        string actualResult = CenterPoint.GetClosest(x1, y1, x2, y2);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+
+    [Test]
+    public void Test_GetClosest_WhenSecondPointIsCloser_ShouldReturnSecondPoint()
+    {
+        // Arrange
+        double x1 = 2d;
+        double y1 = 2d;
+        double x2 = 1d;
+        double y2 = 1d;
+        string expectedResult = "(1, 1)";
+
+        // Act
+        string actualResult = CenterPoint.GetClosest(x1, y1, x2, y2);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+
+    [Test]
+    public void Test_GetClosest_WhenBothPointsHaveEqualDistance_ShouldReturnFirstPoint()
+    {
+        // Arrange
+        double x1 = 2d;
+        double y1 = 2d;
+        double x2 = 2d;
+        double y2 = 2d;
+        string expectedResult = "(2, 2)";
+
+        // Act
+        string actualResult = CenterPoint.GetClosest(x1, y1, x2, y2);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+
+    [Test]
+    public void Test_GetClosest_WhenFirstPointIsNegative_ShouldReturnFirstPoint()
+    {
+        // Arrange
+        double x1 = -1d;
+        double y1 = -1d;
+        double x2 = 2d;
+        double y2 = 2d;
+        string expectedResult = "(-1, -1)";
+
+        // Act
+        string actualResult = CenterPoint.GetClosest(x1, y1, x2, y2);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+
+    [Test]
+    public void Test_GetClosest_WhenSecondPointIsNegative_ShouldReturnSecondPoint()
+    {
+        // Arrange
+        double x1 = 2d;
+        double y1 = 2d;
+        double x2 = -2d;
+        double y2 = -2d;
+        string expectedResult = "(-2, -2)";
+
+        // Act
+        string actualResult = CenterPoint.GetClosest(x1, y1, x2, y2);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
---
+++ Programming_Fundamentals_And_Unit_Testing/ExamPreparation/04-FoldArray-Resources/TestApp.Tests/FoldSumTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class FoldSumTests
+{
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, "5 5 13 13")]
+    [TestCase(new int[] { -1, -2, -3, -4, -5, -6, -7, -8 }, "-5 -5 -13 -13")]
+    [TestCase(new int[] { 0, 0, 0, 0, 0, 0, 0, 0 }, "0 0 0 0")]
+    [TestCase(new int[] { }, "")]
+    [TestCase(new int[] { 1, 2, 0, 0, 0, 0, 7, 8}, "2 1 8 7")]
+    public void Test_FoldArray_ReturnsCorrectString(int[] arr, string expected)
+    {
+        // Arrange
+
+        // Act
+        string actualResult = FoldSum.FoldArray(arr);
+
+        // Assert
+        Assert.AreEqual(expected, actualResult);
+    }
+}

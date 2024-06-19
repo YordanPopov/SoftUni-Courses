69898663cd142c941ad046793d3cbfb744fb9bcd
YordanPopov <jordanpopov190@gmail.com>
2024-06-19 20:20:26 +0300

    Adding new test case tha check odd count of array in FoldArray solution

--- Programming_Fundamentals_And_Unit_Testing/ExamPreparation/04-FoldArray-Resources/TestApp.Tests/FoldSumTests.cs
+++ Programming_Fundamentals_And_Unit_Testing/ExamPreparation/04-FoldArray-Resources/TestApp.Tests/FoldSumTests.cs
@@ -7,6 +7,7 @@ public class FoldSumTests
 {
     [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, "5 5 13 13")]
     [TestCase(new int[] { -1, -2, -3, -4, -5, -6, -7, -8 }, "-5 -5 -13 -13")]
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, "5 5 14 14")]
     [TestCase(new int[] { 0, 0, 0, 0, 0, 0, 0, 0 }, "0 0 0 0")]
     [TestCase(new int[] { }, "")]
     [TestCase(new int[] { 1, 2, 0, 0, 0, 0, 7, 8}, "2 1 8 7")]

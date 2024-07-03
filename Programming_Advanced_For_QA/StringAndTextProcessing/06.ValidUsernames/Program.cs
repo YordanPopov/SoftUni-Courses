bccc2eb0d3838a1d1e114a13ce123cb215fbf945
YordanPopov <jordanpopov190@gmail.com>
2024-07-03 22:34:03 +0300

    Upload StringAndTextProcessing solution

---
+++ Programming_Advanced_For_QA/StringAndTextProcessing/06.ValidUsernames/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Sockets;
+
+namespace _06.ValidUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userNames = Console.ReadLine().Split(", ");
+
+            foreach (string username in userNames)
+            {
+                if (ValidLength(username) && ValidSymbols(username))
+                {
+                    Console.WriteLine(username);
+                }
+            }
+        }
+
+        static bool ValidLength(string username)
+        {
+            if (username.Length >= 3 && 16 >= username.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool ValidSymbols(string username)
+        {
+            bool isValid = false;
+
+            foreach (char c in username)
+            {
+                if ((char.IsLetterOrDigit(c)) || c == '-' || c == '_')
+                {
+                    isValid = true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return isValid;
+        }
+    }
+}

d5fc5c65f93ba7c212bb0311cabded550467253f
YordanPopov <jordanpopov190@gmail.com>
2024-08-15 18:42:32 +0300

    Upload CharToString.js

---
+++ Back_End_Test_Automation/JS_Basics_Exercise/07.CharsToString.js
@@ -0,0 +1,8 @@
+function printString(ch1, ch2, ch3) {
+    let result = ch1 + ch2 + ch3;
+    console.log(result);
+}
+
+printString('a', 'b', 'c');
+printString('%', '2', 'o');
+printString('1', '5', 'p');

6e2384c808114ebd0d626e8add8a6f9d422efd3a
YordanPopov <jordanpopov190@gmail.com>
2024-08-23 19:07:35 +0300

    Upload ReverseAnArray.js

---
+++ Back_End_Test_Automation/JS_Fundamentals/02.ReverseAnArray.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @param {Number} count 
+ * @param {Number[]} arr 
+ */
+function solve(count, arr) {
+    let newArr = arr.slice(0, count);
+
+    console.log(newArr.reverse());
+}
+
+solve(3, [10, 20, 30, 40, 50]);
+solve(4, [-1, 20, 99, 5]);
+solve(2, [66, 43, 75, 89, 47]);

9b33369effe76228f0f7fbdced8be592a8860250
YordanPopov <jordanpopov190@gmail.com>
2024-08-24 16:25:58 +0300

    Upload PersonInfo.js

---
+++ Back_End_Test_Automation/JS_Fundamentals/11.PersonInfo.js
@@ -0,0 +1,14 @@
+function solve(firstName, lastName, age) {
+    let person = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+    };
+
+    return person;
+}
+
+console.log(person);
+
+
+solve('Peter', 'Pan', '20');

911e5000f2cf3740d3daa709d950ef94282570df
YordanPopov <jordanpopov190@gmail.com>
2024-08-31 17:01:13 +0300

    Add SmallestOfThreeNumbers.js

---
+++ Back_End_Test_Automation/JS_Fundamentals_Exercise/07.SmallestOfThreeNumbers.js
@@ -0,0 +1,8 @@
+function findSmallestNumber(num1, num2, num3) {
+    console.log(Math.min(num1, num2, num3));    
+}
+
+findSmallestNumber(2, 5, 3);
+findSmallestNumber(600, 342, 123);
+findSmallestNumber(25, 21, 4);
+findSmallestNumber(2, 2, 2);

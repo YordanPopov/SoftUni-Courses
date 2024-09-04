dd12afec5507aa07206549f2fc5556a431e0c3c9
YordanPopov <jordanpopov190@gmail.com>
2024-09-04 18:20:44 +0300

    Modified Object.js

--- Back_End_Test_Automation/JS_Fundamentals/Demo/Objects.js
+++ Back_End_Test_Automation/JS_Fundamentals/Demo/Objects.js
@@ -37,6 +37,109 @@ for (let key in entries) {
    console.log(key + ' = ' + entries[key]);
 }
 
+// Create an object
+const car = {
+    type: "Fiat",
+    model: "500",
+    color: "white"
+}
+
+const person5 = {};
+
+// Add Properties
+person5.firstName = "John";
+person5.lastName = "Doe";
+person5.age = 50;
+person5.eyeColor = "blue";
+
+// Create an object using new Object()
+const person6 = new Object();
+
+person6.firstName = "John";
+person6.lastName = "Doe";
+person6.age = 50;
+person6.eyeColor = "brown";
+
+console.log(person5.firstName);
+console.log(person6["lastName"]);
+
+// Object Methods
+const person7 = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "black",
+    fullName: function (){
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+// Add properties
+person7.nationality = "English";
+
+// Delete properties
+delete person7.eyeColor;
+
+// nested objects
+
+const obj = {
+    name: "John",
+    age: 30,
+    cars: {
+        car1: "Citroen",
+        car2: "Opel"
+    }
+}
+
+// Display
+console.log(obj.cars.car1);
+
+// Object.values
+const displayArr = Object.values(person)
+console.log(displayArr);
+
+// Object.entries
+const fruits = {
+    Bananas: 300,
+    Oranges: 200,
+    Apples: 500
+}
+
+for (const [fruit, value] of Object.entries(fruits)) {
+    console.log(`${fruit} -> ${value}`);
+}
+
+// JSON.stringify()
+let myString = JSON.stringify(fruits);
+console.log(myString);
+
+// Object constructor
+function Person(firstName, lastName, age, eyeColor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.eyeColor = eyeColor;
+    this.nationality = "English"; // default value
+}
+
+const myFather = new Person("John", "Doe", 50, "brown");
+
+// Adding a propertie to a Constructor
+Person.prototype.car = "VW";
+
+const myFatherInfo = JSON.stringify(myFather);
+console.log(myFatherInfo);
+
+
+
+
+
+
+
+
+
+
+
 
 
 

e892ead8cbb03f40898ca338628c1d9188d966b2
YordanPopov <jordanpopov190@gmail.com>
2024-09-24 19:36:49 +0300

    Modified for Judge system

--- Back_End_Test_Automation/JS_Basics_Excercise/07.CharsToString.js
+++ Back_End_Test_Automation/JS_Basics_Excercise/07.CharsToString.js
@@ -1,8 +1,10 @@
-function printString(ch1, ch2, ch3) {
-    let result = ch1 + ch2 + ch3;
+function printString(arr) {
+    let result = `${arr[2].charCodeAt(0)} ${arr[1].charCodeAt(0)} ${arr[0].charCodeAt(0)}`;
+    let result2 = arr[2] + arr[1] + arr[0];
+    console.log(result2);
     console.log(result);
 }
 
-printString('a', 'b', 'c');
-printString('%', '2', 'o');
-printString('1', '5', 'p');
+printString(['a', 'b', 'c']);
+printString(['%', '2', 'o']);
+printString(['1', '5', 'p']);

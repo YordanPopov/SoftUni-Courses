0583eee05190018ab3e9cc2496f7ee6d99f8500b
YordanPopov <jordanpopov190@gmail.com>
2024-10-05 21:32:47 +0300

    Modified 10.SameNumbers.js

--- Back_End_Test_Automation/JS_Basics_Excercise/10.SameNumbers.js
+++ Back_End_Test_Automation/JS_Basics_Excercise/10.SameNumbers.js
@@ -1,28 +1,44 @@
-function printSum(number) {
+// function printSum(number) {
+//     let sum = 0;
+//     let isSame = true;
+//     let lastDigit = number % 10;
+//     let copyNumber = number;
+
+//     while (copyNumber > 0) {
+//         let digit = copyNumber % 10;
+//         if (digit != lastDigit) {
+//             isSame = false;
+//             break;
+//         }
+
+//         copyNumber = Math.floor(copyNumber / 10);
+//     }
+
+//     while (number > 0) {
+//         let digit = number % 10;
+//         sum += digit;
+//         number = Math.floor(number / 10);
+//     }
+
+//     console.log(isSame);
+//     console.log(sum);
+// }
+
+function printSameNumbers(input) {
+    let numberTotext = input.toString();
+    let teplateNumber = numberTotext[0];
     let sum = 0;
     let isSame = true;
-    let lastDigit = number % 10;
-    let copyNumber = number;
 
-    while (copyNumber > 0) {
-        let digit = copyNumber % 10;
-        if (digit != lastDigit) {
+    for (const digit of numberTotext) {
+        if (digit !== teplateNumber) {
             isSame = false;
-            break;
         }
-
-        copyNumber = Math.floor(copyNumber / 10);
+        sum += parseInt(digit);
     }
 
-    while (number > 0) {
-        let digit = number % 10;
-        sum += digit;
-        number = Math.floor(number / 10);
-    }
-
-    console.log(isSame);
-    console.log(sum);
+    console.log(`${isSame}\n${sum}`);
 }
 
-printSum(2222222);
-printSum(1234);
+printSameNumbers(2222222);
+printSameNumbers(1234);

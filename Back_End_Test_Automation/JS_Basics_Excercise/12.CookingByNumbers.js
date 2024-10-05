3ae051b150fae52171aad6823ba8cc0a69407ef8
YordanPopov <jordanpopov190@gmail.com>
2024-10-05 21:36:34 +0300

    Modified 12.CookingByNumbers.js

--- Back_End_Test_Automation/JS_Basics_Excercise/12.CookingByNumbers.js
+++ Back_End_Test_Automation/JS_Basics_Excercise/12.CookingByNumbers.js
@@ -1,41 +1,20 @@
-function cookNumber(number, op1, op2, op3, op4, op5) {
-    number = Number(number);
+function cookTheNumber(inputArr = []) {
+    let number = Number(inputArr.shift());
 
-    function executeOperation(number, operation) {
-        switch (operation) {
-            case 'chop':
-                number /= 2;
-                break;
-            case 'dice':
-                number = Math.sqrt(number);
-                break; 
-            case 'spice':
-                number++;
-                break;
-            case 'bake':
-                number *= 3;
-                break;
-            case 'fillet':
-                number *= 0.8;
-                break;   
+    for (const op of inputArr) {
+        switch (op) {
+            case 'chop': number /= 2; break;
+            case 'dice': number = Math.sqrt(number); break;
+            case 'spice': number++; break;
+            case 'bake': number *= 3; break;
+            case 'fillet': number *= 0.8; break;
         }
-    
-        return number;
-    }
 
-    number = executeOperation(number, op1);
-    console.log(number);
-    number = executeOperation(number, op2);
-    console.log(number);
-    number = executeOperation(number, op3);
-    console.log(number);
-    number = executeOperation(number, op4);
-    console.log(number);
-    number = executeOperation(number, op5);
-    console.log(number);
+        console.log(number);      
+    }
 }
 
 
 
-cookNumber('32', 'chop', 'chop', 'chop', 'chop', 'chop');
-cookNumber('9', 'dice', 'spice', 'chop', 'bake', 'fillet');
+cookTheNumber(['32', 'chop', 'chop', 'chop', 'chop', 'chop']);
+cookTheNumber(['9', 'dice', 'spice', 'chop', 'bake', 'fillet']);

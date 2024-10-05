76fa7b4bb5a606a74255d455cb480870057ee82b
YordanPopov <jordanpopov190@gmail.com>
2024-10-05 21:13:35 +0300

    Modified 01.Ages.js

--- Back_End_Test_Automation/JS_Basics_Excercise/01.Ages.js
+++ Back_End_Test_Automation/JS_Basics_Excercise/01.Ages.js
@@ -1,22 +1,16 @@
-function printTypeOfPerson(age) {
-
+function printPersonType(age) {
     age = parseInt(age);
-    if (age >= parseInt('0') && age <= parseInt('2')) {
-        console.log('baby');
-    }else if (age > parseInt('2') && age <= parseInt('13')) {
-        console.log('child');
-    }else if (age > parseInt('13') && age <= parseInt('19')) {
-        console.log('teenager');
-    }else if (age > parseInt('19') && age <= parseInt('65')) {
-        console.log('adult');
-    }else if (age > parseInt('65')) {
-        console.log('elder');
-    }else {
-        console.log('out of bounds');
-    }
+
+    let output = (age >= 0 && age <= 2) ? 'baby' :
+                (age >= 3 && age <= 13) ? 'child' : 
+                (age >= 14 && age <= 19) ? 'teenager' :
+                (age >= 20 && age <= 65) ? 'adult' :
+                (age >= 66) ? 'elder' : 'out of bounds';
+
+    console.log(output);
 }
 
-printTypeOfPerson('20');
-printTypeOfPerson('1');
-printTypeOfPerson('100');
-printTypeOfPerson(-'1');
+printPersonType('20');
+printPersonType('1');
+printPersonType('100');
+printPersonType('-1');

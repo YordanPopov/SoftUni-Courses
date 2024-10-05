e0ae10a942d587563a0981274b29cdb3a93a0a1b
YordanPopov <jordanpopov190@gmail.com>
2024-10-05 21:34:20 +0300

    Modified 11.RoadRadar.js

--- Back_End_Test_Automation/JS_Basics_Excercise/11.RoadRadar.js
+++ Back_End_Test_Automation/JS_Basics_Excercise/11.RoadRadar.js
@@ -1,50 +1,42 @@
-function roadRadar(arr) {
-    let speedLimit;
-    let status;
-    let speed = parseInt(arr[0]);
-    let area = arr[1];
-   
-    function printOutput(speed, speedLimit) {
-        let status = '';
-        let difference = 0;
+function CheckSpeedLimit(inputArr) {
+    let currentSpeed = Number(inputArr[0]);
+    let area = inputArr[1];
+    let limit;
+
+    let output = (speed, speedLimit) => {
+        let difference = speed - speedLimit;
+        let status = (difference <= 20) ? 'speeding' :
+                     (difference > 20 && difference <= 40) ? 'excessive speeding' :
+                     'reckless driving';
 
         if (speed <= speedLimit) {
-            console.log(`Driving ${speed} km/h in a ${speedLimit} zone`);
-        }else {
-            difference = speed - speedLimit;
-            if (difference <= 20) {
-                status = 'speeding';
-            }else if (difference > 20 && difference <= 40) {
-                status = 'excessive speeding';
-            }else {
-                status = 'reckless driving';
-            }
-            
-            console.log(`The speed is ${difference} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
+            return `Driving ${speed} km/h in a ${speedLimit} zone`;          
+        } else {
+            return `The speed is ${difference} km/h faster than the allowed speed of ${speedLimit} - ${status}`;
         }
     }
 
     switch (area) {
-        case 'motorway':
-            speedLimit = 130;
-            printOutput(speed, speedLimit);
+        case 'motorway': 
+            limit = 130; 
+            console.log(output(currentSpeed, limit));
             break;
-        case 'interstate':
-            speedLimit = 90;
-            printOutput(speed, speedLimit);
+        case 'interstate': 
+            limit = 90;
+            console.log(output(currentSpeed, limit));
             break;
         case 'city':
-            speedLimit = 50;
-            printOutput(speed, speedLimit);
+            limit = 50;
+            console.log(output(currentSpeed, limit));
             break;
         case 'residential':
-            speedLimit = 20;
-            printOutput(speed, speedLimit);
+            limit = 20;
+            console.log(output(currentSpeed, limit));
             break;
     }
 }
 
-roadRadar(200, 'motorway');
-roadRadar(120, 'interstate');
-roadRadar(40, 'city');
-roadRadar(21, 'residential');
+CheckSpeedLimit([200, 'motorway']);
+CheckSpeedLimit([120, 'interstate']);
+CheckSpeedLimit([40, 'city']);
+CheckSpeedLimit([21, 'residential']);

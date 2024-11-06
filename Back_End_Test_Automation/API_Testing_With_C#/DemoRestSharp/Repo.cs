211ad6262ce40d933e18ff65f8e58ec4a4eb22b4
YordanPopov <jordanpopov190@gmail.com>
2024-11-06 22:06:18 +0200

    Add API Testing With C# solution

---
+++ Back_End_Test_Automation/API_Testing_With_C#/DemoRestSharp/Repo.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoRestSharp
+{
+    public class Repo
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("full_name")]
+        public string FullName { get; set; }
+
+        [JsonProperty("html_url")]
+        public string Html_url { get; set; }
+    }
+}
---
+++ Back_End_Test_Automation/API_Testing_With_C#/SerializingWithSystemTextJson/WeatherForecast.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SerializingWithSystemTextJson
+{
+    public class WeatherForecast
+    {
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
+
+        [JsonPropertyName("temp_c")]
+        public int TemperatureC { get; set; }
+
+        [JsonPropertyName("summary")]
+        public string Summary { get; set; }
+
+        public WeatherForecast(DateTime date, int temp, string summary)
+        {
+            this.Date = date;
+            this.TemperatureC = temp;
+            this.Summary = summary;
+        }
+    }
+}

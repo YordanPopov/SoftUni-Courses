0e48ad686e21a1d47185d12d71e44e465cae4744
YordanPopov <jordanpopov190@gmail.com>
2024-11-12 18:17:52 +0200

    Add some tests

--- Back_End_Test_Automation/Exam_Preparation/Test/OnlineStore.spec.js
+++ Back_End_Test_Automation/Exam_Preparation/Test/OnlineStore.spec.js
@@ -34,7 +34,7 @@ describe("OnlineStore Tests", () => {
             expect(() => onlineStore.isProductAvailable(true, 1)).to.throw("Invalid input.");
             expect(() => onlineStore.isProductAvailable(['foo'], 1)).to.throw("Invalid input.");
             expect(() => onlineStore.isProductAvailable(1111, 1)).to.throw("Invalid input.");
-            expect(() => onlineStore.isProductAvailable({product: "apples"}, 1)).to.throw("Invalid input.");
+            expect(() => onlineStore.isProductAvailable({ product: "apples" }, 1)).to.throw("Invalid input.");
             expect(() => onlineStore.isProductAvailable(NaN, 1)).to.throw("Invalid input.");
             expect(() => onlineStore.isProductAvailable([], 1)).to.throw("Invalid input.");
         });
@@ -43,15 +43,55 @@ describe("OnlineStore Tests", () => {
             expect(() => onlineStore.isProductAvailable('foo', '1')).to.throw("Invalid input.");
             expect(() => onlineStore.isProductAvailable('foo', true)).to.throw("Invalid input.");
             expect(() => onlineStore.isProductAvailable('foo', [1])).to.throw("Invalid input.");
-            expect(() => onlineStore.isProductAvailable('foo', {number: 1})).to.throw("Invalid input.");
+            expect(() => onlineStore.isProductAvailable('foo', { number: 1 })).to.throw("Invalid input.");
             expect(() => onlineStore.isProductAvailable('foo', [])).to.throw("Invalid input.");
             expect(() => onlineStore.isProductAvailable('foo', {})).to.throw("Invalid input.");
         });
     });
 
     describe("canAffordProduct()", () => {
-        it("Test", () => {
-            expect(true).to.be.true;
+        it("Should return correct message if balance is greater than 0 after purchase", () => {
+            let productPrice = 99;
+            let balance = 100;
+            let expectedMsg = `Product purchased. Your remaining balance is $1.`;
+
+            let response = onlineStore.canAffordProduct(productPrice, balance);
+
+            expect(response).to.be.equal(expectedMsg);
+        });
+
+        it("Should return correct message if balance is equal to 0 after purchase", () => {
+            let productPrice = 100;
+            let balance = 100;
+            let expectedMsg = `Product purchased. Your remaining balance is $0.`;
+
+            let response = onlineStore.canAffordProduct(productPrice, balance);
+
+            expect(response).to.be.equal(expectedMsg);
+        });
+
+        it("Should return correct message if balance is less than 0 after purchase", () => {
+            let productPrice = 101;
+            let balance = 100;
+            let expectedMsg = "You don't have sufficient funds to buy this product.";
+
+            let response = onlineStore.canAffordProduct(productPrice, balance);
+
+            expect(response).to.be.equal(expectedMsg);
+        });
+
+        it("Should throw an error if not a number is given for productPrice parameter", () => {
+            expect(() => onlineStore.canAffordProduct(true, 100)).to.throw('Invalid input.');
+            expect(() => onlineStore.canAffordProduct('100', 100)).to.throw("Invalid input.");
+            expect(() => onlineStore.canAffordProduct([1], 100)).to.throw("Invalid input.");
+            expect(() => onlineStore.canAffordProduct({number: 100},100)).to.throw("Invalid input.");
+        });
+
+        it("Should throw an error if not a number is given for accountBalance parameter", () => {
+            expect(() => onlineStore.canAffordProduct(100, true)).to.throw("Invalid input.");
+            expect(() => onlineStore.canAffordProduct(100, '100')).to.throw("Invalid input.");
+            expect(() => onlineStore.canAffordProduct(100, [100])).to.throw("Invalid input.");
+            expect(() => onlineStore.canAffordProduct(100, {balance: 100})).to.throw("Invalid input.");
         });
     });
 

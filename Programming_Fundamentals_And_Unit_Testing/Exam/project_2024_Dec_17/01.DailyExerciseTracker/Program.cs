8ddd015c865b0c34c5ab95ffc8c5622528ad0ded
YordanPopov <jordanpopov190@gmail.com>
2024-11-14 19:49:46 +0200

    Upload project_2024_Dec_17

---
+++ Programming_Fundamentals_And_Unit_Testing/Exam/project_2024_Dec_17/01.DailyExerciseTracker/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _01.DailyExerciseTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysCount = int.Parse(Console.ReadLine());
+            int cumulativeTime = 0;
+
+            if (daysCount <= 0 )
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            for (int day = 1; day <= daysCount; day++)
+            {
+                int exerciseTime = int.Parse(Console.ReadLine());
+                cumulativeTime += exerciseTime;
+                Console.WriteLine(cumulativeTime);
+            }
+        }
+    }
+}
---
+++ Programming_Fundamentals_And_Unit_Testing/Exam/project_2024_Dec_17/02.AverageSales/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _02.AverageSales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dailySales = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int startIndex = int.Parse(Console.ReadLine());
+            int endIndex = int.Parse(Console.ReadLine());
+
+            double totalSales = 0;
+            int counter = 0;
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                totalSales += dailySales[i];
+                counter++;
+            }
+
+            double avrSales = totalSales / counter;
+            Console.WriteLine($"{avrSales:F2}");
+        }
+    }
+}

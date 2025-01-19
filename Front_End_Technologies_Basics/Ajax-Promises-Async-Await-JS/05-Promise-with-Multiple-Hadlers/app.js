8f8dea4d7d48c9880436215ae94c8e595967c398
YordanPopov <jordanpopov190@gmail.com>
2025-01-19 21:34:49 +0200

    Add Promise-with-Multiple-Hadlers task

---
+++ Front_End_Technologies_Basics/Ajax-Promises-Async-Await-JS/05-Promise-with-Multiple-Hadlers/app.js
@@ -0,0 +1,14 @@
+function promiseWithMultipleHandlers() {
+    let prom = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Hello world");
+        }, 2000);
+    });
+
+    prom.then((res) => {
+        console.log(res + " This is first res...");
+        return prom;
+    }).then((res) => {
+        console.log(res + " This is second res...");
+    });
+}

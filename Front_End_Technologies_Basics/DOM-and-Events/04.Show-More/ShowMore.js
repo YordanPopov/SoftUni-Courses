a04fd42fcc5fb0de72a70c8722bd4502d224c2c3
YordanPopov <jordanpopov190@gmail.com>
2025-01-29 21:43:09 +0200

    Add Show-More

---
+++ Front_End_Technologies_Basics/DOM-and-Events/04.Show-More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let aElement = document.querySelector('#more');
+    let hiddenText = document.querySelector('#text');
+
+    aElement.style.display = 'none';
+    hiddenText.style.display = 'inline';
+}

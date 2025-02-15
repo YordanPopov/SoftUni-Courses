b457713beb5de96e78e3e1baabeac361b4f80459
YordanPopov <jordanpopov190@gmail.com>
2025-02-15 11:08:19 +0200

    Add some tests

--- Front_End_Technologies_Basics/Playwright/MemeLounge/tests/Playwright_tests/e2e.test.js
+++ Front_End_Technologies_Basics/Playwright/MemeLounge/tests/Playwright_tests/e2e.test.js
@@ -14,6 +14,13 @@ let user = {
     confirmPass: "123456",
 };
 
+let meme = {
+    title: '',
+    id: "",
+    description: 'This is meme for testing',
+    img: "../images/1.png"
+}
+
 describe("e2e tests", () => {
     beforeAll(async () => {
         browser = await chromium.launch();
@@ -65,6 +72,48 @@ describe("e2e tests", () => {
             await expect(page.locator('a >> text=Logout')).toBeVisible();
         });
 
+        test('Registration with empty fields', async () => {
+            await page.goto(host);
+            await page.click('nav >> text=Register');
+            await page.waitForSelector('#register-form');
+            await page.click('input[type="submit"]');
+
+            await expect(page.locator('#errorBox span', { hasText: 'All fields are required!' })).toBeVisible();
+            await expect(page).toHaveURL(host + '/register');
+        });
+
+        test('Registration with different confirm password', async () => {
+            await page.goto(host);
+            await page.click('text=Register');
+            await page.waitForSelector('form#register-form');
+
+            await page.fill('#username', 'testUser1111');
+            await page.fill('#email', 'testUser1111@email.com');
+            await page.fill('#password', '111111');
+            await page.fill('#repeatPass', '123456');
+            await page.check('input[name="gender"][value="female"]');
+            page.click('input[type="submit"]');
+
+            await expect(page.locator('#errorBox span', { hasText: 'All fields are required!' })).toBeVisible();
+            await expect(page).toHaveURL(host + '/register');
+        });
+
+        test('Registration with existent user', async () => {
+            await page.goto(host);
+            await page.click('text=Register');
+            await page.waitForSelector('form#register-form');
+
+            await page.fill('#username', user.username);
+            await page.fill('#email', user.email);
+            await page.fill('#password', user.password);
+            await page.fill('#repeatPass', user.confirmPass);
+            await page.check('input[name="gender"][value="female"]');
+            page.click('input[type="submit"]');
+
+            await expect(page.locator('#errorBox span', { hasText: 'Existing user or confirm password does not match!' })).toBeVisible();
+            await expect(page).toHaveURL(host + '/register');
+        });
+
         test('Login with valid data', async () => {
             await page.goto(host);
             await page.click('text=Login');
@@ -85,6 +134,42 @@ describe("e2e tests", () => {
             await expect(page).toHaveURL(host + '/catalog');
             await expect(page.locator('.profile >> span')).toHaveText(`Welcome, ${user.email}`);
             await expect(page.locator('a >> text=Logout')).toBeVisible();
+        });
+
+        test('Login with empty fields', async () => {
+            await page.goto(host);
+            await page.click('nav >> text=Login');
+            await page.waitForSelector('#login-form');
+
+            await page.click('input[type="submit"]');
+            await expect(page.locator('#errorBox >> span', { hasText: 'No empty fields' })).toBeVisible();
+            await expect(page).toHaveURL(host + '/login');
+        });
+
+        test('Login with non-existent email', async () => {
+            await page.goto(host);
+            await page.click('a.button >> text=Login');
+            await page.waitForSelector('#login-form');
+
+            await page.fill('#email', 'non-existentUser@email.com');
+            await page.fill('#password', '123456');
+            await page.click('input[type="submit"]');
+
+            await expect(page.locator('#errorBox span'), { hasText: 'Email or password don\'t match' }).toBeVisible();
+            await expect(page).toHaveURL(host + '/login');
+        });
+
+        test('Login with wrong password', async () => {
+            await page.goto(host);
+            await page.click('a.button >> text=Login');
+            await page.waitForSelector('#login-form');
+
+            await page.fill('#email', user.email);
+            await page.fill('#password', '654321');
+            await page.click('input[type="submit"]');
+
+            await expect(page.locator('#errorBox span'), { hasText: 'Email or password don\'t match' }).toBeVisible();
+            await expect(page).toHaveURL(host + '/login');
         });
 
         test('Logout from the application', async () => {
@@ -123,7 +208,7 @@ describe("e2e tests", () => {
             await expect(page.locator('a >> text=Register')).toBeHidden();
         });
 
-        test('Navigation for Guest user', async () => {
+        test('Navigation for Guest user', async ({ page }) => {
             await page.goto(host);
             await expect(page.locator('a >> text=Create Meme')).toBeHidden();
             await expect(page.locator('a >> text=My Profile')).toBeHidden();
@@ -136,6 +221,78 @@ describe("e2e tests", () => {
     });
 
     describe("CRUD", () => {
+        beforeEach(async () => {
+            await page.goto(host);
+            await page.click('nav >> a >> text=Login');
+            await page.waitForSelector('#login-form');
+            await page.fill('#email', user.email);
+            await page.fill('#password', user.password);
+            await page.click('input[type="submit"]');
 
+            await page.waitForURL(host + '/catalog');
+        });
+
+        test('Create a meme with valid data', async () => {
+            await page.click('nav >> a >> text=Create Meme');
+            await page.waitForSelector('#create-form');
+
+            let rnd = Math.floor(Math.random() * 1001);
+            meme.title = `testMeme${rnd}`;
+
+            await page.fill('#title', meme.title);
+            await page.fill('#description', meme.description);
+            await page.fill('#imageUrl', meme.img);
+
+            let [res] = await Promise.all([
+                page.waitForResponse(res => res.url().includes('/data/memes') && res.status() === 200),
+                page.click('input[type="submit"]')
+            ]);
+
+            expect(res.ok()).toBeTruthy();
+            let memeData = await res.json();
+            expect(memeData.title).toBe(meme.title);
+            expect(memeData.description).toBe(meme.description);
+            expect(memeData.imageUrl).toBe(meme.img);
+
+            await expect(page).toHaveURL(host + '/catalog');
+            await expect(page.locator('.meme .card .info p', { hasText: meme.title })).toHaveCount(1);
+        });
+
+        test('Edit a meme with valid data', async () => {
+            await page.click('nav >> a >> text=My Profile');
+            await page.locator('.user-meme .button').first().click();
+            await page.click('.meme-description >> a >> text=Edit');
+            await page.waitForSelector('#edit-form');
+
+            meme.id = page.url().split('/').pop();
+            meme.title += '-Edited';
+            await page.fill('#title', meme.title);
+
+            let [res] = await Promise.all([
+                page.waitForResponse(res => res.url().includes('/data/memes') && res.status() === 200),
+                page.click('input[type="submit"]')
+            ]);
+
+            expect(res.ok()).toBeTruthy();
+            let memeData = await res.json();
+            expect(memeData.title).toBe(meme.title);
+
+            await expect(page).toHaveURL(host + `/details/${meme.id}`);
+            await expect(page.locator('#meme-details h1')).toHaveText(`Meme Title: ${meme.title}s`);
+        });
+
+        test('Delete a meme', async () => {
+            await page.click('nav >> text=My Profile');
+            await page.locator('.user-meme .button').first().click();
+
+            let [res] = await Promise.all([
+                page.waitForResponse(res => res.url().includes('/data/memes') && res.status() === 200),
+                page.click('button >> text=Delete')
+            ]);
+
+            expect(res.ok()).toBeTruthy();
+            await expect(page).toHaveURL(host + '/catalog');
+            await expect(page.locator('.meme .card .info p', { hasText: meme.title })).toHaveCount(0);
+        });
     });
 });

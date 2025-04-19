bb350aeea744c6835e91b75405a12ae851faeb71
YordanPopov <jordanpopov190@gmail.com>
2025-04-19 17:40:55 +0300

    Add sumator tests

---
+++ Front_End_Test-Automation/Appium/SumatorTests/SumatorTests/Pages/SummatorPage.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace SumatorTests.Pages
+{
+	public class SummatorPage
+	{
+		protected AndroidDriver _driver;
+
+		protected WebDriverWait _wait;
+
+		protected Actions actions;
+
+		public SummatorPage(AndroidDriver driver)
+		{
+			_driver = driver;
+			_wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+			actions = new Actions(driver);
+		}
+
+		public AppiumElement firstNumInput => _driver.FindElement(MobileBy.AndroidUIAutomator("new UiSelector().resourceId(\"com.example.androidappsummator:id/editText1\")"));
+
+		public AppiumElement secondNumInput => _driver.FindElement(MobileBy.AndroidUIAutomator("new UiSelector().resourceId(\"com.example.androidappsummator:id/editText2\")"));
+
+		public AppiumElement resultField => _driver.FindElement(MobileBy.AndroidUIAutomator("new UiSelector().resourceId(\"com.example.androidappsummator:id/editTextSum\")"));
+
+		public AppiumElement calcButton => _driver.FindElement(MobileBy.ClassName("android.widget.Button"));
+
+		public void MakeCalc(string num1, string num2)
+		{
+			firstNumInput.Clear();
+			firstNumInput.SendKeys(num1);
+
+			secondNumInput.Clear();
+			secondNumInput.SendKeys(num2);
+
+			actions.MoveToElement(calcButton)
+				.Click()
+				.Perform();
+		}
+
+		public void AssertResult(string result)
+		{
+			Assert.That(resultField.Text, Is.EqualTo(result));
+		}
+	}
+}
---
+++ Front_End_Test-Automation/Appium/SumatorTests/SumatorTests/Tests/SumatorTest.cs
@@ -0,0 +1,91 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Service;
+using SumatorTests.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumatorTests.Tests
+{
+	[TestFixture]
+	public class SumatorTest
+	{
+		private AndroidDriver driver;
+
+		private AppiumLocalService appiumLocalService;
+
+		protected SummatorPage summator;
+
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+			appiumLocalService = new AppiumServiceBuilder()
+				.WithIPAddress("127.0.0.1")
+				.UsingPort(4723)
+				.Build();
+			appiumLocalService.Start();
+
+			var androidOptions = new AppiumOptions();
+			androidOptions.PlatformName = "Android";
+			androidOptions.DeviceName = "AndroidDevice";
+			androidOptions.PlatformVersion = "16";
+			androidOptions.AutomationName = "UiAutomator2";
+			androidOptions.App = "D:\\SoftUni\\Front-End-Testing\\Front-End-Test-Automation-Feb-2025\\Appium\\AppiumLab\\com.example.androidappsummator.apk";
+
+			driver = new AndroidDriver(appiumLocalService.ServiceUrl, androidOptions);
+			summator = new SummatorPage(driver);
+		}
+
+		[TestCase("0", "0", "0")]
+		[TestCase("10", "10", "20")]
+		[TestCase("-10", "-10", "-20")]
+		[TestCase("-10", "10", "0")]
+		[TestCase("10", "-10", "0")]
+		public void Test_Summator_WithIntegerNumbers(string num1, string num2, string expectedResult)
+		{
+			summator.MakeCalc(num1, num2);
+			string actualResult = summator.resultField.Text;
+
+			Assert.That(actualResult, Is.EqualTo(expectedResult));
+		}
+
+		[TestCase("0.1", "0.1", "0.2")]
+		[TestCase("-0.11", "0.254", "0.144")]
+		[TestCase("0.989", "0.4763", "1.4653")]
+		[TestCase("0.99", "0.01", "1.00")]
+		[TestCase("0.99", "0.1", "1.09")]
+		[TestCase("-0.200", "-0.200", "-0.400")]
+
+		public void Test_Summator_WithDecimalNumbers(string num1, string num2, string expectedResult)
+		{
+			summator.MakeCalc(num1, num2);
+			summator.AssertResult(expectedResult);
+		}
+
+		[TestCase("invalidNum", "2", "error")]
+		[TestCase("invalidNum", "invalidNum", "error")]
+		[TestCase("2", "invalidNum", "error")]
+		[TestCase("   ", "123", "error")]
+		[TestCase("123", "", "error")]
+		[TestCase("123", "  ", "error")]
+		[TestCase(".", "0.1", "error")]
+		[TestCase("1", "!", "error")]
+		[TestCase("e2", "2e", "error")]
+		public void Test_Summator_WithInvalidData(string num1, string num2, string expectedResult)
+		{
+			summator.MakeCalc(num1, num2);
+			summator.AssertResult(expectedResult);
+		}
+
+		[OneTimeTearDown]
+		public void OneTimeTearDown()
+		{
+			driver.Quit();
+			driver.Dispose();
+			appiumLocalService.Dispose();
+		}
+	}
+}

d0feede754d2cfad9ffad858aa06bf0b6cd5616b
YordanPopov <jordanpopov190@gmail.com>
2025-04-26 16:16:07 +0300

    Add some tests

--- Front_End_Test-Automation/Appium/ColorNoteTests/ColorNoteTests/Tests/EditNoteTests.cs
+++ Front_End_Test-Automation/Appium/ColorNoteTests/ColorNoteTests/Tests/EditNoteTests.cs
@@ -40,7 +40,6 @@ namespace ColorNoteTests.Tests
 			Assert.That(_homePage.TextNotes.First().Text, Is.EqualTo(lastCreatedNoteTitle));
 
 			string updatedTitle = "EDITED-" + lastCreatedNoteTitle;
-			string updatedContent = "EDITED-" + lastCreatedNoteContent;
 
 			_homePage.TextNotes.First().Click();
 			var noteContent = _driver.FindElement(MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/view_note"));
@@ -53,6 +52,67 @@ namespace ColorNoteTests.Tests
 			_textNotePage.BackBtn.Click();
 
 			Assert.That(_homePage.TextNotes.First().Text, Is.EqualTo(updatedTitle));
+		}
+
+		[Test]
+		public void EditNoteContent()
+		{
+			Assert.That(_homePage.TextNotes.First().Text, Is.EqualTo(lastCreatedNoteTitle));
+
+
+			_homePage.TextNotes.First().Click();
+			Assert.That(_textNotePage.ViewNoteContent.Text, Is.EqualTo(lastCreatedNoteContent));
+
+			string updatedContent = "EDITED-" + lastCreatedNoteContent;
+			_textNotePage.EditBtn.Click();
+			_textNotePage.NoteField.Clear();
+			_textNotePage.NoteField.SendKeys(updatedContent);
+			_textNotePage.DoneBtn.Click();
+			_textNotePage.BackBtn.Click();
+
+			Assert.That(_homePage.TextNotes.First().Text, Is.EqualTo(lastCreatedNoteTitle));
+			_homePage.TextNotes.First().Click();
+			Assert.That(_textNotePage.ViewNoteContent.Text, Is.EqualTo(updatedContent));
+		}
+
+		[Test]
+		public void EditNoteTitleWithEmptyString()
+		{
+			Assert.That(_homePage.TextNotes.First().Text, Is.EqualTo(lastCreatedNoteTitle));
+
+
+			_homePage.TextNotes.First().Click();
+			Assert.That(_textNotePage.ViewNoteContent.Text, Is.EqualTo(lastCreatedNoteContent));
+
+			string updatedTitle = string.Empty;
+			_textNotePage.EditBtn.Click();
+			_textNotePage.TitleField.Clear();
+			_textNotePage.TitleField.SendKeys(updatedTitle);
+			_textNotePage.DoneBtn.Click();
+			_textNotePage.BackBtn.Click();
+
+			var today = DateTime.Now.ToString("M/dd/yy");
+			Assert.That(_homePage.TextNotes.First().Text, Is.EqualTo($"({today})"));
+		}
+
+		[Test]
+		public void EditNoteContentWithEmptyString()
+		{
+			Assert.That(_homePage.TextNotes.First().Text, Is.EqualTo(lastCreatedNoteTitle));
+
+
+			_homePage.TextNotes.First().Click();
+			Assert.That(_textNotePage.ViewNoteContent.Text, Is.EqualTo(lastCreatedNoteContent));
+
+			string updatedContent = string.Empty;
+			_textNotePage.EditBtn.Click();
+			_textNotePage.NoteField.Clear();
+			_textNotePage.NoteField.SendKeys(updatedContent);
+			_textNotePage.DoneBtn.Click();
+			_textNotePage.BackBtn.Click();
+
+			_homePage.TextNotes.First().Click();
+			Assert.That(_textNotePage.ViewNoteContent.Text, Is.Empty);
 		}
 
 		[TearDown]

0c834b69d3f5f31ea8b725d9e494ee976c1e3743
YordanPopov <jordanpopov190@gmail.com>
2025-04-28 21:43:55 +0300

    Add Checklist test and page classes

---
+++ Front_End_Test-Automation/Appium/ColorNoteTests/ColorNoteTests/Pages/ChecklistPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorNoteTests.Pages
+{
+	public class ChecklistPage : BasePage
+	{
+		public ChecklistPage(AndroidDriver driver) : base(driver)
+		{
+
+		}
+
+		public AppiumElement AddItemBtn => _driver.FindElement(MobileBy.AndroidUIAutomator("new UiSelector().resourceId(\"com.socialnmobile.dictapps.notepad.color.note:id/icon\").instance(0)"));
+
+		public AppiumElement TextField => _driver.FindElement(MobileBy.AndroidUIAutomator("new UiSelector().resourceId(\"com.socialnmobile.dictapps.notepad.color.note:id/icon\").instance(0)"));
+
+		public AppiumElement OkayBtn => _driver.FindElement(MobileBy.Id("android:id/button1"));
+
+		public AppiumElement CancelBtn => _driver.FindElement(MobileBy.Id("android:id/button2"));
+
+		public AppiumElement NextBtn => _driver.FindElement(MobileBy.Id("android:id/button3"));
+
+		public IReadOnlyCollection<AppiumElement> Items => _driver.FindElements(MobileBy.XPath("//android.widget.TextView[@resource-id=\"com.socialnmobile.dictapps.notepad.color.note:id/text\" and contains(@text, \"ITEM\")]"));
+	}
+}

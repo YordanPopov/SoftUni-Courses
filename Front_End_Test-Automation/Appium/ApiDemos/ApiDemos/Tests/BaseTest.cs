4832baac9cf5ea33c6509e7e08743f0bb6586756
YordanPopov <jordanpopov190@gmail.com>
2025-05-05 20:58:35 +0300

    Add helmer methods

--- Front_End_Test-Automation/Appium/ApiDemos/ApiDemos/Tests/BaseTest.cs
+++ Front_End_Test-Automation/Appium/ApiDemos/ApiDemos/Tests/BaseTest.cs
@@ -1,4 +1,6 @@
 ﻿using ApiDemos.Drivers;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Interactions;
 
@@ -22,6 +24,32 @@ namespace ApiDemos.Tests
 			_driver.Quit();
 			_driver.Dispose();
 			DriverFactory.StopService();
+		}
+
+		public IWebElement ScrollToElement(string selector)
+		{
+			return _driver.FindElement(MobileBy.AndroidUIAutomator($"new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView({selector})"));
+		}
+
+		public void PerformZoom(int startX1, int startY1, int endX1, int endY1, int startX2, int startY2, int endX2, int endY2)
+		{
+			var finger1 = new PointerInputDevice(PointerKind.Touch);
+			var finger2 = new PointerInputDevice(PointerKind.Touch);
+
+			var moveFinger1 = new ActionSequence(finger1);
+			var moveFinger2 = new ActionSequence(finger2);
+
+			moveFinger1.AddAction(finger1.CreatePointerMove(CoordinateOrigin.Viewport, startX1, startY1, TimeSpan.Zero));
+			moveFinger1.AddAction(finger1.CreatePointerDown(MouseButton.Left));
+			moveFinger1.AddAction(finger1.CreatePointerMove(CoordinateOrigin.Viewport, endX1, endY1, TimeSpan.FromMilliseconds(1000)));
+			moveFinger1.AddAction(finger1.CreatePointerUp(MouseButton.Left));
+
+			moveFinger2.AddAction(finger2.CreatePointerMove(CoordinateOrigin.Viewport, startX2, startY2, TimeSpan.Zero));
+			moveFinger2.AddAction(finger2.CreatePointerDown(MouseButton.Left));
+			moveFinger2.AddAction(finger2.CreatePointerMove(CoordinateOrigin.Viewport, endX2, endY2, TimeSpan.FromMilliseconds(1000)));
+			moveFinger2.AddAction(finger2.CreatePointerUp(MouseButton.Left));
+
+			_driver.PerformActions(new List<ActionSequence> { moveFinger1, moveFinger2 });
 		}
 	}
 }
